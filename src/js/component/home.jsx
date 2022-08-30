d1fffb4577fc0b2bf2e387f9b31922e612b46aa5
tomasherman77 <tomasxherman@gmail.com>
2022-08-30 19:35:29 +0000

    traffic light

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,66 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useState, useEffect} from "react";
 
 //create your first component
 const Home = () => {
+
+	const [ count, setCount] = useState(1);
+	
+	useEffect(()=>{
+		const intervalID = setInterval(()=>{
+				if(count< 4){
+					setCount((count)=>count + 1);
+				}else{
+					setCount(1);
+				}
+		},3000);
+		return () => {
+			clearInterval(intervalID)
+		}
+	  },[count])
+
+	
+	if (count == 1){
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div id="app">
+			<div>
+				<div id="trafficTop"></div>
+				<div id="container">
+					<div class="red light selected"></div>
+					<div class="yellow light"></div>
+					<div class="green light"></div>
+				</div>
+			</div>
 		</div>
-	);
+	)
+	}
+	else if (count == 2){
+		return (
+			<div id="app">
+				<div>
+					<div id="trafficTop"></div>
+					<div id="container">
+						<div class="red light"></div>
+						<div class="yellow light selected"></div>
+						<div class="green light"></div>
+					</div>
+				</div>
+			</div>
+		)
+	}
+	else {
+		return (
+			<div id="app">
+				<div>
+					<div id="trafficTop"></div>
+					<div id="container">
+						<div class="red light"></div>
+						<div class="yellow light"></div>
+						<div class="green light selected"></div>
+					</div>
+				</div>
+			</div>
+		)
+	};
 };
 
 export default Home;
